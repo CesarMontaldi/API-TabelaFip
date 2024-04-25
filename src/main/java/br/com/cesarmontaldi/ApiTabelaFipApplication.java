90843b2366106249eeb4fc90e6379db749cc486f
Cesar <guto_montaldi@yahoo.com.br>
2024-04-25 07:32:40 -0300

    Criação do projeto.

---
+++ src/main/java/br/com/cesarmontaldi/ApiTabelaFipApplication.java
@@ -0,0 +1,13 @@
+package br.com.cesarmontaldi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTabelaFipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTabelaFipApplication.class, args);
+	}
+
+}
